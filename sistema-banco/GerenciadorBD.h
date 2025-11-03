5f33fb93374be010d84d2a0f1cc2e3cef853b473
rcl4-beep <rcl4@cin.ufpe.br>
2025-11-02 22:15:03 -0300

    Update GerenciadorBD.h

--- sistema-banco/GerenciadorBD.h
+++ sistema-banco/GerenciadorBD.h
@@ -9,8 +9,7 @@ class GerenciadorBD {
 private:
     sqlite3* db;
 
-    // --- FUNÇÕES DE TABELA ATUALIZADAS ---
-    // (O seu 'criarTabela' original foi renomeado e um novo foi adicionado)
+
     void criarTabelaContas();
     void criarTabelaUsuarios();
 
@@ -18,14 +17,14 @@ public:
     GerenciadorBD(const std::string& nomeArquivo);
     ~GerenciadorBD();
 
-    // --- Métodos de Conta (que já temos) ---
+   
     bool salvarConta(Conta& conta);
     bool carregarConta(Conta& conta);
 
-    // --- NOVOS MÉTODOS DE USUÁRIO ---
-    // (Estes são os que estão faltando no seu .h)
+    
     bool registrarUsuario(const std::string& usuario, const std::string& senha);
     bool verificarLogin(const std::string& usuario, const std::string& senha);
 };
 
-#endif //SISTEMA_BANCO_GERENCIADORBD_H
+
+#endif
